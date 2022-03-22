17cf16783c711e4a5be9687c7761d1f131247c6d
whit3 <s1091817@gm.pu.edu.tw>
2022-03-22 09:38:16 +0800

    第一次 Commit

---
+++ app/src/main/java/tw/edu/pu/whit3/hello2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.whit3.hello2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
